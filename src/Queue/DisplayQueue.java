73db7e1ca7ff4a88c65498d6fe4ca2874f7aa882
Aniket Pandey <aniketpandey032hit@gmail.com>
2025-12-10 23:41:27 +0530

    We have created a funtion to display a queue.

---
+++ src/Queue/DisplayQueue.java
@@ -0,0 +1,32 @@
+package Queue;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DisplayQueue {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        // Method - 1
+//        System.out.println(q.size());
+//        int x = q.size() ;
+//        for (int i = 0; i < x; i++) {
+//            System.out.print(q.poll()+" ");
+//        }
+        // Method - 2
+        // rear -> 5 4 3 2 1 <- front
+        Queue<Integer> helper = new ArrayDeque<>() ;
+        while(q.size()>0){
+            System.out.print(q.peek()+" ");
+            helper.add(q.poll()) ;
+        }
+        while(helper.size()>0){
+            q.add(helper.poll()) ;
+        }
+    }
+}
