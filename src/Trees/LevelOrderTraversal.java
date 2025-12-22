a8220c90e13158369d38d0df6ca67b338ba427c0
Aniket Pandey <aniketpandey032hit@gmail.com>
2025-12-23 01:03:20 +0530

    Method 2 of level order printing(Or step wise printing).

--- src/Trees/LevelOrderTraversal.java
+++ src/Trees/LevelOrderTraversal.java
@@ -20,6 +20,8 @@ public class LevelOrderTraversal {
         c.right = g;
         System.out.print("Level Order : ");
         levelOrder(a) ;
+        System.out.println("Level Order 2 : ");
+        levelOrder2(a) ;
     }
     private static void levelOrder(TreeNode root){
         Queue<TreeNode> q = new ArrayDeque<>() ;
@@ -31,5 +33,31 @@ public class LevelOrderTraversal {
             if(front.right!=null)q.add(front.right) ;
         }
         System.out.println();
+    }
+    private static void levelOrder2(TreeNode root){
+        int prevlevel = 0 ;
+        Queue<Pair> q = new ArrayDeque<>() ;
+        if(root!=null) q.add(new Pair(root,0)) ;
+        while(q.size()>0){
+            Pair front = q.remove() ;
+            TreeNode temp = front.node ;
+            int lvl = front.level;
+            if(lvl!=prevlevel){
+                System.out.println();
+                prevlevel++ ;
+            }
+            System.out.print(temp.val+" ");
+            if(temp.left!=null)q.add(new Pair(temp.left,lvl+1)) ;
+            if(temp.right!=null)q.add(new Pair(temp.right,lvl+1)) ;
+        }
+        System.out.println();
+    }
+    public static class Pair{
+        TreeNode node ;
+        int level ;
+        Pair(TreeNode node,int level){
+            this.node = node ;
+            this.level = level ;
+        }
     }
 }
