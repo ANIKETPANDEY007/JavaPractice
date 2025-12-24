a9568f5dc711582d28a27632ade964c312fd8333
Aniket Pandey <aniketpandey032hit@gmail.com>
2025-12-25 04:12:35 +0530

    Traversal in a HashSet.

---
+++ src/HashMaps/BasicMap.java
@@ -0,0 +1,16 @@
+package HashMaps;
+
+import java.util.HashMap;
+
+public class BasicMap {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>() ;
+        map.put("Aniket",7) ;
+        map.put("Snehal",8) ;
+        map.put("Abhay",13) ;
+        System.out.println(map+" -> "+map.size());
+        map.remove("Aniket") ;
+        System.out.println(map+" -> "+map.size());
+
+    }
+}
