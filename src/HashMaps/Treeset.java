f8848e78d93eb5bda767919df250342f6d985f86
Aniket Pandey <aniketpandey032hit@gmail.com>
2025-12-26 05:51:39 +0530

    Learned to implement TreeSet.

---
+++ src/HashMaps/Treeset.java
@@ -0,0 +1,14 @@
+package HashMaps;
+import java.util.TreeSet ;
+public class Treeset {
+    public static void main(String[] args) {
+        TreeSet<Integer> set = new TreeSet<>() ;
+        set.add(20) ;
+        set.add(100) ;
+        set.add(10) ;
+        set.add(-2) ;
+        for(int ele: set){
+            System.out.print(ele+" ");
+        }
+    }
+}
