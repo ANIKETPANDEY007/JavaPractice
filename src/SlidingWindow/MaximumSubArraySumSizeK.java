09257ca1d102465252dfe99776fee3bd91f533bf
Aniket Pandey <aniketpandey032hit@gmail.com>
2026-02-05 14:00:30 +0530

    Leared to find the maximum sum of Subarray of size k .

---
+++ src/SlidingWindow/MaximumSubArraySumSizeK.java
@@ -0,0 +1,33 @@
+package SlidingWindow;
+
+public class MaximumSubArraySumSizeK {
+    public static void main(String[] args) {
+        int[] arr = {10,20,1,3,-40,80,10} ;
+        int k=3 ;
+        int n = arr.length;
+        int maxSum = 0 ;
+        // Brute Force .
+//        for (int i = 0; i < n-k+1; i++) {
+//            int sum = 0 ;
+//            for (int j = i; j <= i+k-1; j++) {
+//                sum += arr[j] ;
+//            }
+//            maxSum = Math.max(sum,maxSum) ;
+//        }
+
+        // Sliding Window
+        int i=0,j=k-1,sum=0 ;
+        for (int a = 0; a <= j ; a++) {
+            sum += arr[a] ;
+        }
+        i++ ;
+        j++ ;
+        while(j<n){
+            sum = sum - arr[i-1] + arr[j] ;
+            maxSum = Math.max(sum,maxSum) ;
+            i++ ;
+            j++ ;
+        }
+        System.out.println(maxSum);
+    }
+}
